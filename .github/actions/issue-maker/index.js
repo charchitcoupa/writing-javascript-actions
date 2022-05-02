7f43cce3e2519aceacdf07039ff18956ee07996d
charchitcoupa <charchit.arora@coupa.com>
2022-05-02 15:08:57 -0700

    create action.yml

---
+++ .github/actions/issue-maker/index.js
@@ -0,0 +1,14 @@
+const core = require("@actions/core");
+
+async function run() {
+  try {
+    const issueTitle, jokeBody, token and octokit;
+    issues.create();
+  }
+  catch {
+    console.log("something went wrong");
+    core.setFailed;
+  }    
+}
+
+run();
