f004d28cd8cb3ad2e2f3bfb1274defa969e7be1e
charchitcoupa <charchit.arora@coupa.com>
2022-05-02 15:10:12 -0700

    create action.yml

--- .github/actions/issue-maker/index.js
+++ .github/actions/issue-maker/index.js
@@ -1,14 +1,23 @@
 const core = require("@actions/core");
+const github = require("@actions/github");
 
 async function run() {
   try {
-    const issueTitle, jokeBody, token and octokit;
-    issues.create();
+    const issueTitle = core.getInput("issue-title");
+    const jokeBody = core.getInput("joke");
+    const token = core.getInput("repo-token");
+
+    const octokit = new github.getOctokit(token);
+
+    const newIssue = await octokit.rest.issues.create({
+      repo: github.context.repo.repo,
+      owner: github.context.repo.owner,
+      title: issueTitle,
+      body: jokeBody
+    });
+  } catch (error) {
+    core.setFailed(error.message);
   }
-  catch {
-    console.log("something went wrong");
-    core.setFailed;
-  }    
 }
 
 run();
